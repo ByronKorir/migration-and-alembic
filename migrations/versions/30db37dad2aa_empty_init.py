0e25957aa5b0d5240731216fe962ce71f67cd206
byron korir <byron.korir@student.moringaschool.com>
2023-12-10 12:13:11 +0300

    complete

---
+++ migrations/versions/30db37dad2aa_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 30db37dad2aa
+Revises: 
+Create Date: 2023-12-08 14:24:18.518343
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '30db37dad2aa'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
